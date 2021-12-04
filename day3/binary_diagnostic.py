f140171662446f457076d9366c7ae948801ebc40
suzm10 <suzanmanasreh@gmail.com>
2021-12-03 20:00:14 -0500

    day3 help

--- day3/binary_diagnostic.py
+++ day3/binary_diagnostic.py
@@ -1,11 +1,12 @@
 def part1():
     file = open("day3/binary_diagnostic.py", "r")
     for line in file:
-        digits = line.split()
-        map_object = map(int, digits)
-        list_of_digits = list(map_object)
-
-
-        print(list_of_digits[0])
+        A = 1
+        # create a digits list
+        digits = []
+        for i in range(0, len(line), A):
+            # convert to int, after the slicing process
+            digits.append(int(line[i : i + A]))
+        print(digits[0])
 
 part1()
